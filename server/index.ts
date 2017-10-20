2dea6f2d35cece9593eec8db28f24449b7baa4c2
Emily Sharp <emilysharp@Emilys-MacBook-Air.local>
2017-10-20 15:30:11 -0500

    implemented basic site

---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import { join } from 'path';
+import * as express from 'express';
+
+const clientPath = join(__dirname, "../client");
+const app = express();
+
+app.use(express.static(clientPath));
+
+console.log(process.env.GREETING);
+app.listen(3000, () => {
+	console.log("I'm listening");
+})
